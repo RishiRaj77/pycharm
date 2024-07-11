58ac8bf643a5e07c3d63165cbe746944546b7013
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-12 00:55:30 +0530

    "[add]day11"

--- day14.py
+++ day14.py
@@ -1,11 +1,73 @@
-def my_function():
-    for i in range(1,20):
-        if i == 20:
-         print('yesss!!!')
-my_function()
-
 import random
-dice_images = ["1","2","3","4","5","6"]
-dice_num = random.randint(1,5)
-print(dice_images[dice_num])
+data = [
+	{
+		'name': 'Instagram',
+		'follower_count': 346,
+		'description': 'Social media platform',
+		'country': 'United States'
+	},
+	{
+		'name': 'Cristiano Ronaldo',
+		'follower_count': 215,
+		'description': 'Footballer',
+		'country': 'Portugal'
+	},
+	{
+		'name': 'Ariana Grande',
+		'follower_count': 183,
+		'description': 'Musician and actress',
+		'country': 'United States'
+	},
 
+	{
+		'name': 'Cristiano Ronaldo',
+		'follower_count': 215,
+		'description': 'Footballer',
+		'country': 'india'
+	},
+	{
+		'name': ' Ronaldo',
+		'follower_count': 15,
+		'description': 'Footballer',
+		'country': 'brazil'
+	},
+	{
+		'name': 'Cris',
+		'follower_count': 21,
+		'description': 'actor',
+		'country': 'nagaland'
+	},
+	{
+		'name': 'neymar',
+		'follower_count': 190,
+		'description': 'Footballer',
+		'country': 'brazil'
+	},
+]
+
+	# account_name =account_a ["name"]
+	# account_desc =account_a["description"]
+	# account_country =account_a["country"]
+	# print(f" {account_name} ,a { account_desc} ,from {account_country}")
+	#
+	# account_name =account_b ["name"]
+	# account_desc =account_b["description"]
+	# account_country =account_b["country"]
+	# print(f" {account_name} ,a { account_desc} ,from {account_country}")
+
+# 52 se leake 61 tak hum log function create kar sakte hai
+def account_format(account):
+	account_a = random.choice(data)
+	account_b = random.choice(data)
+	if account_a == account_b:
+		account_b = random.choice(data)
+	account_name =account["name"]
+	account_desc =account["description"]
+	account_country =account["country"]
+	print(f" {account_name} ,a { account_desc} ,from {account_country}")
+
+def check_answer(guess,account_a,account_b)
+print(f" compare a  : {account_format(account_a)}")
+print(f" compare b  : {account_format(account_b)}")
+
+guess = input(" who has more followers A OR B : ")
