fe339cc876c80fe279911e19b291c7956fad7306
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-11 23:37:53 +0530

    "[add]day11"

---
+++ day14.py
@@ -0,0 +1,11 @@
+def my_function():
+    for i in range(1,20):
+        if i == 20:
+         print('yesss!!!')
+my_function()
+
+import random
+dice_images = ["1","2","3","4","5","6"]
+dice_num = random.randint(1,5)
+print(dice_images[dice_num])
+
