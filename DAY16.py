222e0a712e8640dea3724ca4de3408d0c6b16603
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-17 23:06:25 +0530

    "[add]day11"

--- DAY16.py
+++ DAY16.py
@@ -1,7 +1,25 @@
 
-import another_module
 
-print(another_module.another_varible)
+# from turtle import Turtle ,Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+#
+# timmy.forward(150)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
 
+from prettytable import PrettyTable
+table = PrettyTable()
 
-import turtle
+table.field_names = [" BIKE ", "ability"]
+table.add_row([" SHO ", " FLAME "])
+table.add_row([" KIOCHI ", " THUNDER "])
+table.add_row([" GABU ", " IMPACT "])
+
+print(table.align)
+print(table)
+
--- day17.py
+++ day17.py
@@ -1,23 +1,55 @@
-from menu import Menu
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+# from menu import Menu
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+#
+# money_machine = MoneyMachine()
+# coffee_maker = CoffeeMaker()
+# menu = Menu()
+#
+# is_on = True
+#
+# while is_on:
+#     options = menu.get_items()
+#     choice = input(f"What would you like? ({options}): ")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#             coffee_maker.make_coffee(drink)
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-menu = Menu()
 
-is_on = True
+class User :
+    def __init__(self):
+        print("new user created")
 
-while is_on:
-    options = menu.get_items()
-    choice = input(f"What would you like? ({options}): ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink = menu.find_drink(choice)
+user_1 = User()
+user_1.id =" 2084"
+user_1.name = " rishi "
+print(user_1.id)
 
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+user_2 = User()
+user_2.id =" 204"
+user_2.name = " andyrishi "
+print(user_2.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
