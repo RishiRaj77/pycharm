a60acef6d325968b9679d0e1a77846ccdf359263
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-25 00:31:25 +0530

    "[add]day11"

--- day22_ping_pong.py
+++ day22_ping_pong.py
@@ -1,5 +1,7 @@
 from turtle import Turtle , Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 # Create a turtle screen
 screen = Screen()
@@ -8,8 +10,9 @@ screen.bgcolor("black")
 screen.title("ping_pong")
 screen.tracer(0)
 
-r_paddle = Paddle(350,0)
-l_paddle = Paddle(-350,0)
+r_paddle = Paddle(350 , 0)
+l_paddle = Paddle(-350 , 0)
+ball = Ball ()
 
 
 screen.listen()
@@ -21,11 +24,9 @@ screen.onkey(l_paddle.go_down(),"w")
 
 is_game_is_on = True
 while is_game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 
 screen.exitonclick()
-
-
-
-
-
