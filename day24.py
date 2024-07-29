65766c3863e3441b9334528e42607b6dc41bbecc
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-30 00:34:23 +0530

    "[add]day11"

---
+++ day24.py
@@ -0,0 +1,32 @@
+# with open ( "weather-data.csv") as data_file:
+#     data = data_file.readline()
+#     print(data)
+#
+# import csv
+#
+# with open ( "weather-data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature =[]
+#     for row in data:
+#         print(row)
+
+#
+# import panda as pd
+#
+# data = pd.read_csv(" weather-data.csv ")
+# print(data)
+
+# import panda
+#
+# # From a dictionary
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+# df = panda.DataFrame(data)
+# print(df)
+
+
+
+
