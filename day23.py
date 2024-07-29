0d462826a83447a4d13b9c306036ff5bffcc6bfc
RishiRaj77 <rishirajdubey72@gmail.com>
2024-07-29 23:53:01 +0530

    "[add]day11"

--- day23.py
+++ day23.py
@@ -1,9 +1,28 @@
-class Employee:
-    pass
+
+for i in range(1,5+1):
+    for j in range(1,i+1):
+        print( j,end= " ")
+
+    print(" ")
+
+n=int(input("enter the num"))
+for i in range(n+1):
+    for j in range(i):
+        print("*",end= " ")
+
+    print(" ")
 
 
-b = Employee()
-a = Employee()
-print(a)
-print(b)
-print("hello")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print( j,end= " ")
+
+    print(" ")
+
+
+for i in range(0,5):
+    for j in range(5,i,-1):
+        print( j,end= " ")
+
+    print(" ")
+
