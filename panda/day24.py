59bdb7699aeb715d07ba5dea0635efcf38021d3f
RishiRaj77 <rishirajdubey72@gmail.com>
2024-08-05 22:52:31 +0530

    "[add]day11"

--- panda/day24.py
+++ panda/day24.py
@@ -11,20 +11,43 @@
 #         print(row)
 
 #
-# import panda as pd
+# import pandas as pd
 #
 # data = pd.read_csv(" weather-data.csv ")
 # print(data)
 
+# import pandas
+#
+# # From a dictionary
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+# df = pandas.DataFrame(data)
+# print(df)
+
 import pandas
-
-# From a dictionary
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
-}
-df = pandas.DataFrame(data)
-print(df)
-
-
+# #
+data = pandas.read_csv("weather-data.csv")
+# data_dict = data.to_dict()
+# print(data_dict)
+# temp_list = data["temp"].to_list
+# print(temp_list)
+# (temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# print(data["temp"].argmax())
+#
+#
+# print(data["condition"])
+# print(data.condition)
+#
+# print(data["temp"])
+# print(data.temp)
+# print(data["temp"].max())
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+# print(data[data.condition == "Rain"])
+print(data[data.temp == "Monday"])
