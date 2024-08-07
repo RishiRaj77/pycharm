117335d4c22f82cf1fafae0f7c9b619edf5a7e62
RishiRaj77 <rishirajdubey72@gmail.com>
2024-08-07 21:58:03 +0530

    "[add]day11"

--- panda/tute course.py
+++ panda/tute course.py
@@ -1,65 +1,65 @@
-# c = int(input("celcuis : "))
-# f = (c * 1.8) + 32
-# print(f)
-# lst = [1,2,3,3,4,6]
-# print(max(lst))
-# print(min(lst))
-# print(sum(lst))
-# print(abs(-56557))
-# print(pow(2,3))
-# a = int(input(" enter the number : "))
-# if (a > 4 ):
-#     print(" a is greater than 4")
-# elif (a >= 4 ):
-#     print(" a is eqal to 4")
-# else:
-#     print("not greater than 4")
+c = int(input("celcuis : "))
+f = (c * 1.8) + 32
+print(f)
+lst = [1,2,3,3,4,6]
+print(max(lst))
+print(min(lst))
+print(sum(lst))
+print(abs(-56557))
+print(pow(2,3))
+a = int(input(" enter the number : "))
+if (a > 4 ):
+    print(" a is greater than 4")
+elif (a >= 4 ):
+    print(" a is eqal to 4")
+else:
+    print("not greater than 4")
 
-# lst =[1,2,3]
-# sum_of_num = 0
-#
-# for i in lst:
-#     sum_of_num = sum_of_num + i
-# print(sum_of_num)
-#
-# city =["mumbai" , "pune ", "ujjain"]
-#
-# for i in range(len(city)):
-#     print(city[2])
+lst =[1,2,3]
+sum_of_num = 0
 
-# marks = {'Ram': 90, 'Shayam': 55, 'Sujit': 77}
-#
-# for student in marks:
-#     if student == 90 :
-#         print(marks[student])
-# else:
-#     print ('No entry with that name found.')
+for i in lst:
+    sum_of_num = sum_of_num + i
+print(sum_of_num)
+
+city =["mumbai" , "pune ", "ujjain"]
+
+for i in range(len(city)):
+    print(city[2])
+
+marks = {'Ram': 90, 'Shayam': 55, 'Sujit': 77}
+
+for student in marks:
+    if student == 90 :
+        print(marks[student])
+else:
+    print ('No entry with that name found.')
 
 
-# n= 3
-# sum_of_num = 0
-#
-# i =1
-# while i<=n:
-#     sum_of_num = sum_of_num +i
-#     i = i+1
-# print(sum_of_num)
+n= 3
+sum_of_num = 0
 
-# i = 1
-# while i <= 5:
-#     print("heloo")
-#     i = i + 1
-#
-# i = 5
-# while i >= 1:
-#     print("heloo")
-#     i = i - 1
-# for i in range(3,33,3):
-#     print(i)
+i =1
+while i<=n:
+    sum_of_num = sum_of_num +i
+    i = i+1
+print(sum_of_num)
 
-# for i in range(3,8):
-#     print(i)
-#     if i == 5:
-#         continue
-#     else:
-#         print(i)
+i = 1
+while i <= 5:
+    print("heloo")
+    i = i + 1
+
+i = 5
+while i >= 1:
+    print("heloo")
+    i = i - 1
+for i in range(3,33,3):
+    print(i)
+
+for i in range(3,8):
+    print(i)
+    if i == 5:
+        continue
+    else:
+        print(i)
